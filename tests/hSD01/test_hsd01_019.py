382bea455aac7548f65f21956188c0f01937b928
Jomari Pantorilla <jompantorilla@gmail.com>
2024-10-22 15:21:06 +0800

    Added tests for hSD01-019 on QnA scenarios.
    Ammended test for hSD01-017 to target the correct event.

--- tests/hSD01/test_hsd01_019.py
+++ tests/hSD01/test_hsd01_019.py
@@ -6,36 +6,37 @@ from app.gameengine import EventType
 from tests.helpers import *
 
 
-class Test_hSD01_017(unittest.TestCase):
+class Test_hSD01_019(unittest.TestCase):
   engine: GameEngine
   player1: str
+
 
   def setUp(self):
     initialize_game_to_third_turn(self)
 
-  
-  def test_hSD01_017_need_at_least_one_card_other_than_manechan_to_activate(self):
-    # QnA: Q14 (2024.09.21)
+
+  def test_hSD01_019_can_only_archive_cheers_on_stage_not_on_cheer_deck(self):
+    # QnA: Q15 (2024.09.21)
 
     engine = self.engine
 
     p1: PlayerState = engine.get_player(self.player1)
 
-    # Setup to have mane-chan on hand with no other cards
-    p1.hand = []
-    add_card_to_hand(self, p1, "hSD01-017")
+    # Setup to have amazing pc on hand without cheers on stage
+    add_card_to_hand(self, p1, "hSD01-019")
+
+    p1.archive_attached_cards(p1.get_cheer_ids_on_holomems())
 
     """Test"""
     self.assertEqual(engine.active_player_id, self.player1)
-    self.assertEqual(len(p1.hand), 1)
 
+    reset_mainstep(self)
     main_step_event = engine.all_events[-1]
-    self.assertEqual(main_step_event["active_player"], self.player1)
-
-    validate_actions(self, main_step_event["available_actions"],[
+    validate_actions(self, main_step_event["available_actions"], [
+      GameAction.MainStepBloom for _ in range(2 * (len(p1.backstage) + len(p1.center)))
+    ] + [
       GameAction.MainStepCollab for _ in range(len(p1.backstage))
     ] + [
-      GameAction.MainStepBatonPass,
       GameAction.MainStepBeginPerformance,
       GameAction.MainStepEndTurn
-    ]) # no available action to use mane-chan
+    ]) # no action available to activate amazing pc since no cheer on stage
